acc5d13a7f8ac3f37fcb6fcfcf9370acac4a710c
Chris Barrick <cbarrick1@gmail.com>
2014-04-17 11:20:22 -0400

    Problem 1

---
+++ p1.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var Set = require('collections/set');
+
+
+// Solution: p1(3, 5, 1000) === 233168
+
+var p1 = module.exports = function (multiple1, multiple2, cap) {
+	var a = multiple1;
+	var b = multiple2;
+	var sum = 0;
+	var cache = new Set();
+
+	for (var i = a, j = b; i < cap || j < cap; i += a, j += b) {
+		if (i < cap && !cache.has(i)) {
+			sum += i;
+			cache.add(i);
+		}
+		if (j < cap && !cache.has(j)) {
+			sum += j;
+			cache.add(j);
+		}
+	}
+
+	return sum;
+}
